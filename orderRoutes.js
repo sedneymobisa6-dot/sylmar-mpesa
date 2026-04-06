123f43f593788dd3d66621c8cf5550119fed6ff2
sedneymobisa6-dot <sedneymobisa6@gmail.com>
2026-04-06 21:00:22 +0300

    Delete orderRoutes.js

--- orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { checkout, getOrder } = require("../controllers/orderController");
-
-router.post("/checkout", checkout);
-router.get("/order/:id", getOrder);
-
-module.exports = router;
